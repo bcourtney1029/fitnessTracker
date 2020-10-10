1cb4853b4c005a49f7163f67e0b9543fed94c8e2
Bryan Courtney <bcourtney1029@gmail.com>
2020-10-10 15:02:49 -0500

    attempting to fix DB connection issues

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,52 +1,61 @@
 const db = require("../models/workout.js");
-const router = require("express").Router();
 
 module.exports = (app) => {
 
-    app.use(function timeLog(req, res, next) {
-        console.log("Time: ", Date.now());
-        next();
-    });
+  
 
     app.get("/api/workouts", (req, res) => {
-      Workout.find({}, (err, workouts) => {
-          if(err){
-              console.log(err);
-          } else {
-              res.json(workouts)
-          }
-      });
+      db.Workout.find().then(dbWorkout => {
+        dbWorkout.forEach(workout => {
+            var total = 0;
+            workout.exercises.forEach(e => {
+                total += e.duration;
+            });
+            workout.totalDuration = total;
+
+        });
+
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
   });
 
-    app.post("/api/workouts", (req, res) => {
-        Workout.create({})
-          .then(newWorkout => {
-            res.json(newWorkout);
-          })
-          .catch(err => {
-            res.json(err);
-          });
+  app.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body).then((dbWorkout => {
+        res.json(dbWorkout);
+    })).catch(err => {
+        res.json(err);
     });
-      
-    app.put("/api/workouts/:workout", ({ params, body }, res) => {
-      Workout.findOneAndUpdate(
-                                { _id: params.id},
-                                {$push: {excercises:body }},
-                                { upsert: true, useFindandModify:false},
-                                updatedWorkout => {
-                                    res.json(updatedWorkout);
-                                })
   });
       
-    app.get("/api/workouts/range", (req, res) => {
-        Workout.find({})
-          .then(dbWorkout => {
-            res.json(dbWorkout);
-          })
-          .catch(err => {
-            res.json(err);
+    app.put("/api/workouts/:id", (req, res) => {
+      db.Workout.findOneAndUpdate(
+          { _id: req.params.id },
+          {
+              $inc: { totalDuration: req.body.duration },
+              $push: { exercises: req.body }
+          },
+          { new: true }).then(dbWorkout => {
+              res.json(dbWorkout);
+          }).catch(err => {
+              res.json(err);
           });
+
+  });
+      
+  app.get("/api/workouts/range", (req, res) => {
+
+    db.Workout.find({}).then(dbWorkout => {
+        console.log("ALL WORKOUTS");
+        console.log(dbWorkout);
+
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
     });
+
+  });
 };
 
 
